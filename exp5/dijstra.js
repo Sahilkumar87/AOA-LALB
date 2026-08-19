aaf2d9cfecc4c46360e15a5e940a8108af1e02de
sahil kumar <sahilkumar06804@gmail.com>
2026-08-19 14:59:57 +0530

    dijstra done!

---
+++ exp5/dijstra.js
@@ -0,0 +1,15 @@
+var dijstra = (adjust = [], ) => {
+    adjust[0].push(2, 6);
+    adjust[0].push(1, 13);
+
+    adjust[1].push(2, 3);
+    adjust[1].push(0, 1);
+
+    adjust[2].push(0, 6);
+    adjust[2].push(1, 3);
+
+
+    
+
+
+}
